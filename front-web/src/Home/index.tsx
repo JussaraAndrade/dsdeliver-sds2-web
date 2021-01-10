b405da5e1728091b2f3187f94f3f45f6951ac22b
JussaraAndrade <jussara.jandrade1993@outlook.com>
2021-01-09 21:01:20 -0300

    Layout settings

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -8,7 +8,7 @@ function Home (){
     <>
     <div className="home-container">
             <div className="home-content">
-              <div className="home-acetions">
+              <div className="home-actions">
                   <h1 className="home-title">
                   Faça seu pedido <br/>que entregamos <br/> pra você!!!
                   </h1>
